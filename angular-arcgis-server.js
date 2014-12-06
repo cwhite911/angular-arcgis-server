62171e386078f2d981c3ec2348a87db12822a650
Corey White <smortopahri@gmail.com>
2014-12-06 16:00:28 -0500

    Added server constructor class and reset method

--- angular-arcgis-server.js
+++ angular-arcgis-server.js
@@ -4,7 +4,28 @@
     'ngCookies',
     'ngResource'
   ]);
-  app.factory('agsFactory', ['$resource', function($resource){
-    
-  }]);
+  app.factory('agsFactory', ['$resource', '$cacheFactory',
+    function($resource, $cacheFactory){
+      //Create connection to ArcGIS REST Services Directory
+      var Server = function (conn){
+        this.conn = {
+          protocol: conn.protocol || 'http://',
+          host: conn.host || '',
+          subdomain: conn.subdomain || '',
+          domain: conn.domain,
+          port: conn.port || '',
+          path: conn.path || '/arcgis/rest/services'
+        };
+        return this;
+      };
+      Server.prototype = {
+        //Method that allows base server connections to be reset
+        resetConn: function (conn){
+          angular.extend(this.conn, conn);
+          return this;
+        }
+      };
+      return (Server);
+    }
+  ]);
 });
