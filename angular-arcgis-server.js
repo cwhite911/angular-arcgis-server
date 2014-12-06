f3a8653c515dfcd6070de46c718a548de9b9db4e
Corey White <smortopahri@gmail.com>
2014-12-05 23:48:32 -0500

    Set up basic project structure

---
+++ angular-arcgis-server.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+  var app = angular.module('angular-arcgis-server', [
+    'ngCookies',
+    'ngResource'
+  ]);
+  app.factory('agsFactory', ['$resource', function($resource){
+    
+  }]);
+});
