4f230c5273eba3f2253c6b8e3d36d9031c2c8b50
cwhite911 <smortopahri@gmail.com>
2014-12-22 14:56:14 -0500

    Updated readme.md

--- README.md
+++ README.md
@@ -1,27 +1,61 @@
-angular-arcgis-server
-=====================
+angular-arcgis-server (In Development)
+======================================
 
 Utility for ArcGIS Server
 
-## In Development
+## Usage
 
+Inject module and add factory to controller
 
-## Examples
+```javascript
+
+  angular.module('app', ['agsserver']).
+    controller('test', [ 'Ags', function(Ags){}]);
+
+```
+
+Create ArcGIS server object
+
+```javascript
+
+  //Create new server object
+  var testServer = new Ags({'host': <Your Host> });
+
+```
+
+Define options
+
+```javascript
+
+  //Setup options
+    var options = {
+      folder: <folder>,
+      layer: <layer>,
+      service: <service>,
+      server: 'FeatureServer' || 'MapServer' || 'GPServer',,
+      params: {
+        f: 'json',
+        where: 'OBJECTID > 0'
+        },
+        headers: {
+          'Content-Type': 'text/plain'
+          },
+          timeout: 5000,
+          method: 'GET',
+          geojson: true,
+          actions: 'query'
+        };
+
+```
 
 Example options settings
 
 ```javascript
-options = {
-    folder: 'GEWA',
-    layer: 'Streams',
-    service: 'gewa_sde',
-    server: 'FeatureServer' || 'MapServer' || 'GPServer',
-    actions: 'query',
-    params: {},
-    header: {
-      'Content-Type': undefined
-    },
-    timeout: 5000
-  };
+
+  //Make request to Server
+  testServer.request(options)
+    .then(function(data){
+      //Do something
+  });
 
 ```
--- app.js
+++ app.js
@@ -61,55 +61,6 @@ angular.module('app', ['agsserver']).
       console.log(data);
       $scope.boundary = data;
     });
-    // console.log(testServer.request(options));
-    // //
-    // console.log(testServer.request(options1));
-    // var makePromiseWithSon = function(options) {
-	  //       // This service's function returns a promise, but we'll deal with that shortly
-	  //      testServer.request(options)
-	  //           // then() called when son gets back
-	  //           .then(function(data) {
-	  //               // promise fulfilled
-	  //               if (data) {
-	  //                   console.log(data);
-	  //               } else {
-	  //                   console.log('bad');
-	  //               }
-	  //           }, function(error) {
-	  //               // promise rejected, could log the error with: console.log('error', error);
-	  //               console.log({issue: 'Promise Rejected', error: error});
-	  //           });
-	  //   };
-    //
-    //
-    //   $scope.boundary = makePromiseWithSon(options1);
-    //   // makePromiseWithSon(options);
 
-
-
-
-// var hello =  testServer.request(options);
-// console.log(hello);
-// hello.then(function(data){
-//   console.log(data);
-// });
-    // promise.then(function(data){
-    //   console.log('look at all the data');
-    //   console.log(data);
-    // });
-
-    // console.log(testServer.request(options));
-
-
-    //TODO- CREATE server parse
-    //Example:
-    //Create Server
-    //    server = new Ags('exampel.com');
-    //
-    //////Get features////////////////////////
-
-    //    Server.getLayer('folder', 'layer/table', options).then(function (data){
-    //       --Do Something
-    //    });
 
   }]);
