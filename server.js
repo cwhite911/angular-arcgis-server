25a798fb554806b515ac96ba384c6c218f4f19f3
cwhite911 <smortopahri@gmail.com>
2014-12-30 09:14:39 -0500

    Made change to server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
 var app = express();
-app.use(express.static(__dirname)).listen(8245);
-console.log('Listening on port 8245');
+app.use(express.static(__dirname))
+  .listen(8245, function(){
+    console.log('Listening on port 8245');
+  });
