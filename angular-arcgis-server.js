144298ebd7fb736662ebf4552f43bb749e4a38f4
cwhite911 <smortopahri@gmail.com>
2015-03-26 16:23:33 -0400

    Remove authRequest method

--- angular-arcgis-server.js
+++ angular-arcgis-server.js
@@ -334,15 +334,29 @@
             console.log(err);
           }
           var that = this,
+              config,
               //Get host
-              baseUrl = that.getConn(),
+              baseUrl = that.getConn();
               //Set config
-              config = {
-                params: {f:'json'},
-                cache: base
-              },
+              if (options.params.token){
+                config = {
+                  headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                  params: {
+                    f:'json',
+                    token: options.params.token
+                  },
+                  cache: base
+                };
+              }
+              else{
+                config = {
+                  params: {f:'json'},
+                  cache: base
+                };
+            }
+
               //Set url
-              url = this.serviceUrl || baseUrl + '/' + options.folder + '/' + options.service + '/' + options.server;
+            var  url = this.serviceUrl || baseUrl + '/' + options.folder + '/' + options.service + '/' + options.server;
 
           //Gets the base of the ArcGIS server structure
           return $http.get(url, config).then(function(res){
@@ -446,101 +460,6 @@
 
                 return deferred.promise;
 
-          },
-
-          //Request with token
-          authRequest: function (options){
-
-            var that = this,
-                //Get host
-                baseUrl = that.getConn(),
-                //Set config
-                config = {
-                  headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                  params: {
-                    f:'json',
-                    token: options.params.token
-                  },
-                  cache: base
-                },
-
-                //Set url
-                url = this.serviceUrl || baseUrl + '/' + options.folder + '/' + options.service + '/' + options.server;
-
-            //Gets the base of the ArcGIS server structure
-            return $http.get(url, config).then(function(res){
-                if (typeof res.data === 'object' && !res.data.error) {
-                  //Concat layers and tables array
-                  var _layers = res.data.layers.concat(res.data.tables);
-                  //set layers if layer has not been set
-                  that.layers = that.layers.length === 0 ? [{
-                    folder: options.folder,
-                    service: [{
-                      name: options.service,
-                      server: options.server,
-                      layers: _layers
-                    }]
-                  }] : that.layers;
-
-                  //Checks if layer option is set if not is checks action tpye
-                  if (!options.layer){
-                    switch (options.actions){
-                      case 'find':
-                      case 'identify':
-                      case 'export':
-                        if (options.params.layers){
-                          options.params.layers = getLayerIdList(_layers, options.params.layers);
-                        }
-                        break;
-                      default:
-                        return;
-                    }
-                    //Returns undefinded if no layer is set
-                    return;
-                  }
-                  //If layer option is set returns layer id
-                  else {
-                    //Gets the layer id for requested layer
-                    var layerId = getLayerId(_layers, options.layer);
-                    //Checks if layerId is a number and returns it else it rejects the promise
-                    if(typeof layerId === 'number'){
-                      return layerId;
-                    }else{
-                      return  $q.reject(res.data);
-                    }
-                  }
-  	            }
-                else {
-  	            // invalid response
-                  console.log(res.data.error);
-  	               return $q.reject(res.data);
-  	            }
-              }, function(res){
-                return $q.reject(res.data);
-              })
-              .then(function(layerId){
-                // Request parameters
-              var req = that.setRequst(url, layerId, options);
-                //Make request to server and return promise
-                return $http(req)
-                .then(function(res){
-                  if (typeof res.data === 'object') {
-                    if (options.geojson === true){
-                      return geojsonTools.toGeojson(res.data);
-                    }
-                    return res.data;
-                  }
-                  else {
-                    // invalid response
-                    console.log('invalide response');
-                    return $q.reject(res.data);
-                  }
-                }, function(res){
-                  //Something went wrong
-                  console.log({error: 'Promise rejected - Check your options and server'});
-                  return $q.reject(res.data);
-                });
-              });
           }
         };
       //Returns server contructor class
