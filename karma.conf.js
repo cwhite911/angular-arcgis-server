755f430573a1ef3a7a4c91c277bafaab879f6c3b
cwhite911 <smortopahri@gmail.com>
2015-09-10 12:56:23 -0400

    Added firefox to karma

--- karma.conf.js
+++ karma.conf.js
@@ -61,11 +61,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
